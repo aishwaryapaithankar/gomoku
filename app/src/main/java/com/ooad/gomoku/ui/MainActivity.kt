8bfc1f898e0a49024a0d85f97491a37a636659f4
Varad Raut <varadraut@gmail.com>
2022-11-16 19:57:19 -0700

    added report

--- app/src/main/java/com/ooad/gomoku/ui/MainActivity.kt
+++ app/src/main/java/com/ooad/gomoku/ui/MainActivity.kt
@@ -9,6 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
