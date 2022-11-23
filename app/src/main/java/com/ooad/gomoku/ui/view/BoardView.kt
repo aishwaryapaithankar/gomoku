2c5f99467a8650e06cf0359f6987c49bfb413a9f
Varad Raut <varadraut@gmail.com>
2022-11-22 17:05:22 -0700

    Logic to display board lines and pieces

--- app/src/main/java/com/ooad/gomoku/ui/view/BoardView.kt
+++ app/src/main/java/com/ooad/gomoku/ui/view/BoardView.kt
@@ -6,16 +6,33 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import com.ooad.gomoku.R
+import kotlin.math.floor
 import kotlin.math.min
+
+enum class Piece {
+    NONE,
+    BLACK,
+    WHITE
+}
 
 class BoardView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private var boardSize: Int
     private var boardMeasure: Float
     private var boardLen: Int = 0
-    private var boardLinePaint: Paint
+
+    private var blackPaint: Paint
+    private var whitePaint: Paint
+    private var blackBorderPaint: Paint
+
+    private var logicalBoard: Array<Array<Piece>>
+
+    // debug - delete later
+    private var count = 0
 
     init {
         context.theme.obtainStyledAttributes(attrs, R.styleable.BoardView, 0, 0).apply {
@@ -27,14 +44,24 @@ class BoardView(context: Context, attrs: AttributeSet) : View(context, attrs) {
             }
         }
 
-        boardLinePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        logicalBoard = Array(boardSize) { Array(boardSize) { Piece.NONE } }
+
+        blackPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             color = Color.BLACK
+            strokeWidth = 3f
+        }
+        whitePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply { color = Color.WHITE }
+        blackBorderPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = Color.BLACK
+            strokeWidth = 2f
+            style = Paint.Style.STROKE
         }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         drawLines(canvas)
+        drawPieces(canvas)
     }
 
     private fun drawLines(canvas: Canvas) {
@@ -42,12 +69,37 @@ class BoardView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         var y = 0f
         for (i in 0..boardSize) {
             // vertical lines
-            canvas.drawLine(x, 0f, x, boardLen.toFloat(), boardLinePaint)
+            canvas.drawLine(x, 0f, x, boardLen.toFloat(), blackPaint)
             x += boardLen.toFloat() / boardSize
             // horizontal lines
-            canvas.drawLine(0f, y, boardLen.toFloat(), y, boardLinePaint)
+            canvas.drawLine(0f, y, boardLen.toFloat(), y, blackPaint)
             y  += boardLen.toFloat() / boardSize
         }
+    }
+
+    private fun drawPieces(canvas: Canvas) {
+        val cOffset = boardLen.toFloat() / (boardSize * 2)
+        val cRadius = cOffset * 0.8f
+
+        for (row in logicalBoard.indices) {
+            for (col in logicalBoard[row].indices) {
+                val piece = logicalBoard[row][col]
+                if (piece != Piece.NONE) {
+                    val cx = (row.toFloat() * boardLen / boardSize) + cOffset
+                    val cy = (col.toFloat() * boardLen / boardSize) + cOffset
+                    val paint = if (piece == Piece.WHITE) whitePaint else blackPaint
+                    canvas.drawCircle(cx, cy, cRadius, paint)
+                    canvas.drawCircle(cx, cy, cRadius, blackBorderPaint)
+                }
+            }
+        }
+    }
+
+    fun addPiece(x: Int, y: Int, piece: Piece) {
+        if (logicalBoard[x][y] != Piece.NONE) return
+        logicalBoard[x][y] = piece
+        invalidate()
+        requestLayout()
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -55,6 +107,36 @@ class BoardView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         Log.i(TAG, "onSizeChanged: w=$w, h=$h")
         boardLen = min(w, h)
     }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return gestureDetector.onTouchEvent(event)
+    }
+
+    private fun getBoardLocation(x: Float, y: Float) : Pair<Int, Int> {
+        val xIndex = floor(x / boardLen * boardSize)
+        val yIndex = floor(y / boardLen * boardSize)
+        Log.i(TAG, "xIndex=$xIndex, yIndex=$yIndex")
+        return xIndex.toInt() to yIndex.toInt()
+    }
+
+    private val gestureListener = object : GestureDetector.SimpleOnGestureListener() {
+        override fun onDown(e: MotionEvent): Boolean {
+            return true
+        }
+
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.i(TAG, "x=${e.x}, y=${e.y}")
+            val (x, y) = getBoardLocation(e.x, e.y)
+            if (x < 0 || x >= boardSize || y < 0 || y >= boardSize)
+                return true
+
+            count++
+            addPiece(x, y, if (count % 2 == 0) Piece.WHITE else Piece.BLACK)
+            return true
+        }
+    }
+
+    private val gestureDetector = GestureDetector(context, gestureListener)
 }
 
 private const val TAG = "BoardView"
